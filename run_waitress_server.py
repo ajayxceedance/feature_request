73265098493f5dae6ae0ade1dc52a0bbb56fa046
ajayxceedance <36442482+ajayxceedance@users.noreply.github.com>
2018-03-07 11:20:08 +0530

    Update run_waitress_server.py

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -1,6 +1,6 @@
 import os
 from waitress import serve
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FeatureRequest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feature_request.settings")
 application = get_wsgi_application()
 serve(application,host="0.0.0.0",port=os.environ["PORT"])
